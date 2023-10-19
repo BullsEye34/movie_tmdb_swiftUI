bd45fa599ccc29a0ed9d304293d19ffafe85f68e
P Vamshi Prasad <vamvijkum@gmail.com>
2023-10-19 06:25:45 +0530

    Initial Commit

---
+++ MovieTMDB/MovieTMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTMDBApp.swift
+//  MovieTMDB
+//
+//  Created by P Vamshi Prasad on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieTMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
